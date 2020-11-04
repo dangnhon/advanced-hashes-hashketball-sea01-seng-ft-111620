08d76d542bd333af6d77ca78924162fec86b136c
Nhon Dang <dang.nhon12345@yahoo.com>
2020-11-04 17:44:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,7 +219,8 @@ def big_shoe_rebounds
       if attribute == :players
         data.max_by do |player_stat|
             player_stat[:shoe]
-            #binding.pry
+            
+            binding.pry
             return player_stat[:shoe]
 
         end
